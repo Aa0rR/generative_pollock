0a3b3052d05d3e8376f28213b638f83ad0a15f07
D3ltaPowers <42669682+D3ltaPowers@users.noreply.github.com>
2022-02-23 16:58:17 +0100

    Do Defs

--- draw.py
+++ draw.py
@@ -1,79 +1,80 @@
 import turtle
 import random
-import os 
-import sys
+#import os 
+#import sys
 from tkinter import *
 from tkinter import colorchooser
-#8ed6cc
-#f1baa4
+
+
+a = turtle.Turtle()
+b = turtle.Turtle()
+c = turtle.Turtle()
 
 color_code = {}
 def choose_color(name):
     color_code['rgb'], color_code['hex'] = colorchooser.askcolor(title ="Choose the "+name+" color",color="")
     return color_code['hex']
+
+def upThePens():
+   a.up()
+   b.up()
+   c.up()
  
-#def setPosition(line):
-#   line.setpos(random.randint(0,1000),random.randint(0,1000))
+def downThePens():
+   a.down()
+   b.down()
+   c.down()
+def setPosition():
+   a.setpos(random.randint(0,1000),random.randint(0,1000))
+   b.setpos(random.randint(0,1000),random.randint(0,1000))
+   c.setpos(random.randint(0,1000),random.randint(0,1000))
 
 bg=turtle.Screen()
 bg.bgcolor(choose_color("background"))
 
 colors = [choose_color("first"), choose_color("second"),choose_color("third"), choose_color("fourth")]
 
-a = turtle.Turtle()
-b = turtle.Turtle()
-c = turtle.Turtle()
 a.hideturtle()
 b.hideturtle()
 c.hideturtle()
 
-a.up()
-b.up()
-c.up()
+upThePens()
+
 while(True):
- a.setpos(random.randint(0,1000),random.randint(0,1000))
- b.setpos(random.randint(0,1000),random.randint(0,1000))
- c.setpos(random.randint(0,1000),random.randint(0,1000))
- #setPosition(b)
- #setPosition(c)
  
- a.down()
- b.down()
- c.down()
+   setPosition()
+   downThePens()
 
- a.pensize(random.randint(5, 10))
- a.speed(random.randint(4000, 5000))
- a.color(random.choice(colors))
+   a.pensize(random.randint(5, 10))
+   a.speed(random.randint(40000, 50000))
+   a.color(random.choice(colors))
 
- b.pensize(random.randint(5, 10))
- b.speed(random.randint(4000, 5000))
- b.color(random.choice(colors))
+   b.pensize(random.randint(5, 10))
+   b.speed(random.randint(40000, 50000))
+   b.color(random.choice(colors))
 
- c.pensize(random.randint(5, 10))
- #a.goto(random.randint(x/2, x),random.randint(y/2, y))
- c.speed(random.randint(4000, 5000))
- c.color(random.choice(colors))
+   c.pensize(random.randint(5, 10))
+   c.speed(random.randint(4000, 50000))
+   c.color(random.choice(colors))
 
- if(random.randint(1, 50) % 2) == 0:
-    a.rt(random.randint(45, 270))
-    b.rt(random.randint(45, 270))
-    c.rt(random.randint(45, 270))
- else:
-    a.lt(random.randint(45, 270))
-    a.circle(random.randint(20, 5000))
-    b.lt(random.randint(45, 270))
-    b.circle(random.randint(20, 5000))
-    c.lt(random.randint(45, 270))
-    c.circle(random.randint(20, 5000))
+   if(random.randint(1, 50) % 2) == 0:
+      a.rt(random.randint(45, 270))
+      b.rt(random.randint(45, 270))
+      c.rt(random.randint(45, 270))
+   else:
+      a.lt(random.randint(45, 270))
+      a.circle(random.randint(20, 5000))
+      b.lt(random.randint(45, 270))
+      b.circle(random.randint(20, 5000))
+      c.lt(random.randint(45, 270))
+      c.circle(random.randint(20, 5000))
 
- fa = random.randint(25, 1000)
- fb = random.randint(25, 100)
- fc = random.randint(25, 100)
+   fa = random.randint(25, 1000)
+   fb = random.randint(25, 100)
+   fc = random.randint(25, 100)
 
- a.fd(fa)
- b.fd(fb)
- c.fd(fc)
-
- a.up()
- b.up()
- c.up()
+   a.fd(fa)
+   b.fd(fb)
+   c.fd(fc)
+   
+   upThePens()
